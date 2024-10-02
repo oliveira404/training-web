408640b067eb88d233206444d7aac7e55cb83f9f
oliveira404 <lrmcpe22@gmail.com>
2024-10-02 01:09:22 -0300

    Criando a action de cadastro de serviço

--- multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/web/controllers/ServicoController.java
@@ -1,24 +1,36 @@
 package br.com.treinaweb.ediaristas.web.controllers;
 
+import org.checkerframework.checker.units.qual.s;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.com.treinaweb.ediaristas.core.enums.Icone;
 import br.com.treinaweb.ediaristas.core.models.Servico;
+import br.com.treinaweb.ediaristas.core.repositories.ServicoRepository;
 
 @Controller
 @RequestMapping(value = "/admin/servicos")
 public class ServicoController {
+
+    @Autowired
+    private ServicoRepository repository;
     
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
-        var modelAndView = new ModelAndView("form");
+        var modelAndView = new ModelAndView("/admin/servico/form");
         modelAndView.addObject("servico", new Servico());
         //modelAndView.addObject("icones", Icone.values());
-
         return modelAndView;
+    }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico) {
+        repository.save(servico);
+        return "redirect:/admin/servicos/cadastrar";
     }
 
     @ModelAttribute("icones")

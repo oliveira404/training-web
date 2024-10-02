ef12cdcd97cd0f1fc1948915ad74abddc6fbf6f8
oliveira404 <lrmcpe22@gmail.com>
2024-10-02 12:40:09 -0300

    Criando o Mapper de serviço

--- multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/core/repositories/ServicoRepository.java
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/core/repositories/ServicoRepository.java
@@ -1,10 +1,8 @@
 package br.com.treinaweb.ediaristas.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import br.com.treinaweb.ediaristas.core.models.Servico;
 
-@Repository
 public interface ServicoRepository extends JpaRepository<Servico, Long>{
     
 }

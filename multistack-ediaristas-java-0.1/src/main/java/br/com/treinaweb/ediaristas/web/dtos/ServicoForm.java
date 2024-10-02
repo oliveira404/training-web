41a4757623e5e61f762d235b6ec8804ed14563ee
oliveira404 <lrmcpe22@gmail.com>
2024-10-02 12:21:08 -0300

    Adicionando camada de DTO com validação

---
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/web/dtos/ServicoForm.java
@@ -0,0 +1,90 @@
+package br.com.treinaweb.ediaristas.web.dtos;
+
+import java.math.BigDecimal;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+import br.com.treinaweb.ediaristas.core.enums.Icone;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServicoForm {
+    
+    @Size(min = 3, max = 50)
+    @NotNull
+    private String nome;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorMinimo;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer qtdHoras;
+
+    @NotNull
+    @Max(100)
+    @PositiveOrZero
+    private BigDecimal porcentagemComissao;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer horasQuarto;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorQuarto;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer horasSala;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorSala;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer horasBanheiro;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorBanheiro;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer horasCozinha;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorCozinha;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer horasQuintal;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorQuintal;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer horasOutros;
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal valorOutros;
+
+    @NotNull
+    private Icone icone;
+
+    @NotNull
+    @Positive
+    private Integer posicao;
+}

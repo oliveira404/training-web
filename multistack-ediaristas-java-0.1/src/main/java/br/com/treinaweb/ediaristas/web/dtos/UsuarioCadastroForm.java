de6a6389e404dfa4f88bb54a0d9d066bf6234df0
oliveira404 <lrmcpe22@gmail.com>
2024-10-07 16:14:46 -0300

    adicionando e configurando o mapstruct

---
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/web/dtos/UsuarioCadastroForm.java
@@ -0,0 +1,32 @@
+package br.com.treinaweb.ediaristas.web.dtos;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioCadastroForm {
+    
+    @NotNull
+    @Size(min = 3, max = 255)
+    private String nomeCompleto;
+   
+    @Email
+    @NotNull
+    @Size(min = 3, max = 255)
+    private String email;
+        
+    @NotNull
+    @NotEmpty
+    private String senha;
+
+    @NotNull
+    @NotEmpty
+    private String confirmacaoSenha;
+}

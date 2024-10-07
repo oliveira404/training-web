da9c72fac3d629c99f97f23ccdd476274fa03374
oliveira404 <lrmcpe22@gmail.com>
2024-10-07 14:41:01 -0300

    Criação do usuario entity e o repository do mesmo.

---
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/core/models/Usuario.java
@@ -0,0 +1,44 @@
+package br.com.treinaweb.ediaristas.core.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import br.com.treinaweb.ediaristas.core.enums.TipoUsuario;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class Usuario {
+
+    @Id
+    @ToString.Include
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_completo", nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Column(name = "tipo_usuario", length = 8, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TipoUsuario tipoUsuario;
+    
+}
---
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/core/repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.treinaweb.ediaristas.core.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.treinaweb.ediaristas.core.models.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}

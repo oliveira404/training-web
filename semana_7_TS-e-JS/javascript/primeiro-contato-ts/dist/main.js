66d1a9a4073d57d52d726c0eabad9f50e97161ac
oliveira404 <lrmcpe22@gmail.com>
2024-10-11 18:30:35 -0300

    enum

--- semana_7_TS-e-JS/javascript/primeiro-contato-ts/dist/main.js
+++ semana_7_TS-e-JS/javascript/primeiro-contato-ts/dist/main.js
@@ -1,18 +1,12 @@
 "use strict";
-var pessoa;
-pessoa = {
-    nome: "akita",
-    idade: 27
-};
-console.log(pessoa);
-let a;
-let qualquer;
-let array = [];
-qualquer = "cola";
-qualquer = 10;
-array.push("cobra");
-a = 5;
-console.log(5);
-var nome;
-nome = "rafael";
-console.log(nome);
+var Direction;
+(function (Direction) {
+    Direction["ADMIN"] = "ADMIN";
+    Direction["USER"] = "USER";
+})(Direction || (Direction = {}));
+function handleMovement(direction) {
+    if (direction === Direction.ADMIN) {
+        console.log(direction);
+    }
+}
+handleMovement(Direction.ADMIN);
--- semana_7_TS-e-JS/javascript/primeiro-contato-ts/scripts/main.ts
+++ semana_7_TS-e-JS/javascript/primeiro-contato-ts/scripts/main.ts
@@ -1,35 +1,12 @@
-type Pessoa = {
-  nome: string;
-  idade: number;
-};
-
-var pessoa: Pessoa;
-
-pessoa = {
-  nome: "akita",
-  idade: 27
+enum Direction {
+  ADMIN = 'ADMIN',
+  USER = 'USER'
 }
 
-console.log(pessoa);
+function handleMovement(direction: Direction) {
+  if (direction === Direction.ADMIN) {
+    console.log(direction);
+  }
+}
 
-
-
-
-let a: number;
-
-let qualquer: string | number;
-let array: string[] = [];
-
-qualquer = "cola";
-qualquer = 10;
-
-array.push("cobra");
-
-a = 5;
-
-console.log(5);
-
-var nome: string;
-nome = "rafael";
-console.log(nome);
-
+handleMovement(Direction.ADMIN);

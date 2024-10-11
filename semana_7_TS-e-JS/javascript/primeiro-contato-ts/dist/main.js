5778f5c7218d96f495c92982d05420ff8ee30882
oliveira404 <lrmcpe22@gmail.com>
2024-10-11 17:53:12 -0300

    Instalação do TypeScript

---
+++ semana_7_TS-e-JS/javascript/primeiro-contato-ts/dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+let a;
+a = 5;
+console.log(5);
+var nome;
+nome = "rafael";
+console.log(nome);
---
+++ semana_7_TS-e-JS/javascript/primeiro-contato-ts/scripts/main.ts
@@ -0,0 +1,10 @@
+let a: number;
+
+a = 5;
+
+console.log(5);
+
+var nome: string;
+nome = "rafael";
+console.log(nome);
+

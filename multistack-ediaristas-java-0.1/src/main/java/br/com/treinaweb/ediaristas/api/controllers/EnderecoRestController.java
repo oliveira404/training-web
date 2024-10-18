036336cdfd79c8977a62ba382580bdad0f6c1555
oliveira404 <lrmcpe22@gmail.com>
2024-10-18 14:30:36 -0300

    Implementando a rota de busca de endereço por CEP

---
+++ multistack-ediaristas-java-0.1/src/main/java/br/com/treinaweb/ediaristas/api/controllers/EnderecoRestController.java
@@ -0,0 +1,22 @@
+package br.com.treinaweb.ediaristas.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.treinaweb.ediaristas.core.services.consultaendereco.DTOs.EnderecoResponse;
+import br.com.treinaweb.ediaristas.core.services.consultaendereco.adapters.EnderecoService;
+
+@RestController
+@RequestMapping("/api/enderecos")
+public class EnderecoRestController {
+    
+    @Autowired
+    private EnderecoService service;
+
+    @GetMapping
+    public EnderecoResponse buscarEnderecoPorCep(@RequestParam(required = false) String cep) {
+        return service.buscarEnderecoPorCep(cep);
+    }
+}
